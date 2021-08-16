4f8445b055c8ce0125102114b6051ec7036c1657
Victor Costa ( Desenvolvedor Web ) <70709626+victor9743@users.noreply.github.com>
2021-08-16 19:11:21 -0300

    Add files via upload

---
+++ EntradaSaidaDados/EntradaSaidaDados/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EntradaSaidaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Saida de dados 
+            Console.WriteLine("Hello World!");
+
+               Entrada de Dados
+
+            string mensagem = Console.ReadLine();*/
+
+            Console.WriteLine("Digite Um texto");
+
+            string n1 = Console.ReadLine();
+
+            Console.WriteLine("Mensagem Digitada: " + n1);
+
+
+
+           
+
+        }
+    }
+}
