95d4992b325ea0fedc527c47874e3f43e830b720
Victor Costa ( Desenvolvedor Web ) <70709626+victor9743@users.noreply.github.com>
2021-08-16 19:43:49 -0300

    Add files via upload

---
+++ ConversaoVariaveis/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConversaoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Conversão de variáveis
+            /*
+             string para inteiro 
+            int numero;
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(numero);
+
+            inteiro para string
+            int mensagem = 123456;
+            String numString = Convert.ToString(mensagem);
+
+            Console.WriteLine("Número Convertido " + numString);
+
+            float para inteiro 
+            float num = 123456;
+
+            int numFloat = Convert.ToInt32(num);
+
+            Console.WriteLine(numFloat);
+
+            boolean para string */
+            bool sucesso = true;
+
+            string mensagemSucesso = Convert.ToString(sucesso);
+
+            Console.WriteLine(mensagemSucesso);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+        }
+    }
+}
