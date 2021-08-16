21c8915acd9dfa88be91f8c6d6d5f3f506fb69c4
Victor Costa ( Desenvolvedor Web ) <70709626+victor9743@users.noreply.github.com>
2021-08-16 19:59:26 -0300

    Add files via upload

---
+++ EstruturaLogica/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EstruturaLogica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* if / Else / Else if exemplo */
+            float nota1, nota2, nota3, media;
+
+            Console.WriteLine("Digite a Primeira Nota");
+            nota1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Segunda Nota");
+            nota2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Terceira Nota");
+            nota3 = float.Parse(Console.ReadLine());
+
+            media = (nota1 + nota2 + nota3) / 3;
+
+            if(media >= 7)
+            {
+       
+                Console.WriteLine("Aluno Aprovado Média: " + media);
+
+            }
+            else if(media <7 && media >= 4)
+            {
+                Console.WriteLine("Aluno em Recuperação Média: " + media);
+            }
+            else
+            {
+                Console.WriteLine("Aluno Reprovado Média: " + media);
+            }
+
+
+
+
+
+        }
+    }
+}
