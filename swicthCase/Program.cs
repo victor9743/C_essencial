2f7c6d59388c77824bb83c6f48a13d456eb7bb6a
Victor Costa ( Desenvolvedor Web ) <70709626+victor9743@users.noreply.github.com>
2021-08-16 20:57:35 -0300

    Add files via upload

---
+++ swicthCase/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace swicthCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = { "Opção 1", "Opção 2", "Opção 3", "Opção 4" };
+            int op;
+
+           
+                op = Convert.ToInt32(Console.ReadLine());
+
+                switch(op)
+                {
+
+                    case 1:
+                        Console.WriteLine(vet[op]);
+                    break;
+                    case 2:
+                        Console.WriteLine(vet[op]);
+                    break;
+                    case 3:
+                        Console.WriteLine(vet[op]);
+                    break;
+                    case 4:
+                        Console.WriteLine(vet[op]);
+                    break;
+                    default:
+                        Console.WriteLine("Opção Inválida");
+                        break;
+
+
+
+
+                }
+            
+
+        }
+    }
+}
