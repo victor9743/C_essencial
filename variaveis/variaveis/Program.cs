f12da245f16b17416d66ee128755896f6d8102a0
Victor Costa ( Desenvolvedor Web ) <70709626+victor9743@users.noreply.github.com>
2021-08-16 19:19:34 -0300

    Add files via upload

---
+++ variaveis/variaveis/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Variáveis */
+
+            // string
+                string mensagem = "Hello Wolrd";
+
+                Console.WriteLine(mensagem);
+
+            // inteiro
+                int numero = 10;
+                Console.WriteLine(numero);
+
+            // float / double
+                float numFloat = 10;
+                Console.WriteLine(numFloat);
+
+                double numDouble = 10.6;
+                Console.WriteLine(numDouble);
+
+            // boolean  ( Lógico )
+                bool aprender = true;
+                Console.WriteLine(aprender);
+
+                bool desistir = false;
+                Console.WriteLine(desistir);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
