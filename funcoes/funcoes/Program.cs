2367466f4f8bae4aa19edef4c8d23a09195df785
Victor Costa ( Desenvolvedor Web ) <70709626+victor9743@users.noreply.github.com>
2021-08-17 11:19:37 -0300

    Add files via upload

---
+++ funcoes/funcoes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             chamada de metodo
+             mensagem();
+            */
+            /* chamada com retorno exemplo */
+
+            Console.WriteLine("Digite o salário");
+            float salario = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Novo Salário é de: " + calculo(salario));
+
+        }
+        /* Método sem retorno
+        static void mensagem()
+        {
+            Console.WriteLine("Hello Wolrd");
+            return;
+        }
+            método com retorno
+        */
+        static float calculo(float valor){
+
+            float aumento = 5;
+            float salarioCalculo =+ (valor * aumento) / 100;
+            float novoSalario = salarioCalculo + valor;
+            return novoSalario;
+
+
+           
+        }
+    }
+}
