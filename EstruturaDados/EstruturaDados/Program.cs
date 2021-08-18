38db43292f2ac1c1179e22075475f16899702d93
Victor Costa ( Desenvolvedor Web ) <70709626+victor9743@users.noreply.github.com>
2021-08-18 12:10:22 -0300

    Add files via upload

---
+++ EstruturaDados/EstruturaDados/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+// bibliotecas utilizadas para usar o HASH
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EstruturaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* HASH */
+            string SourceData;
+
+            byte[] tmpSource;
+            byte[] tmpHash;
+
+            SourceData = "1234";
+            tmpSource = ASCIIEncoding.ASCII.GetBytes(SourceData);
+
+            tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+
+            Console.WriteLine(ByteArrayToString(tmpHash));
+            static string ByteArrayToString(byte[] arrInput)
+            {
+                int i;
+                StringBuilder sOutput = new StringBuilder(arrInput.Length);
+                for (i = 0; i < arrInput.Length; i++)
+                {
+                    sOutput.Append(arrInput[i].ToString("X2"));
+                }
+                return sOutput.ToString();
+            }
+
+
+        }
+    }
+}
